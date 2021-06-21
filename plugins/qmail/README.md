f6c413c5dd78fd71652363ae6b9ffae8397c6be2
m3rlinux <m3rlinux.it@gmail.com>
2021-06-21 11:03:59 +0200

    qmail 1.0

---
+++ plugins/qmail/README.md
@@ -0,0 +1,58 @@
+# Qmail
+
+Check_mk Qmail monitoring scripts
+
+
+### Table of Contents
+**[Getting Started](#getting-started)**<br>
+**[Prerequisites](#prerequisites)**<br>
+**[Installing package](#installing-package)**<br>
+**[Installing agent plugin](#installing-agent-plugin)**<br>
+**[License](#license)**<br>
+
+
+## Getting Started
+
+These instructions will get you the qmail plugin correctly installed on your Check_MK Server and Qmail Server.
+
+### Prerequisites
+
+#### On Host to be monitored
+- [x] python2.7
+
+### Installing package
+
+1. Connect via SSH on your monitoring server;
+1. change user to your site;
+1. Download the qmail-1.0.mkp; `wget|curl`
+1. use cmk to install de package.
+
+
+#### Example
+```
+# su - mysite
+~ wget https://github.com/m3rlinux/check_mk/raw/master/check_mk/plugins/qmail/qmail-1.0.mkp
+~ cmk -P install qmail-1.0.mpk
+```
+
+
+### Installing agent plugin
+
+1. Connect via SSH on your Qmail server;
+1. download the plugin; `wget|curl`
+1. move the plugin into "plugins" folder of your agent;
+1. add execution permission to the file.
+
+#### Example
+
+```
+# wget https://github.com/m3rlinux/check_mk/raw/master/check_mk/plugins/qmail/plugins/mk_qmail
+# mv mk_qmail /usr/lib/check_mk_agent/plugins/
+# chmod +x /usr/lib/check_mk_agent/plugins/cloudmark
+```
+
+## License
+
+This project is licensed under the GPLv3 License. [See more details in license file](../../../LICENSE)
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
---
+++ plugins/qmail/wato/qmail_queue_length.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+# Check_MK Qmail Queue Plugin
+#
+# Copyright 2021, Davide Gibilisco <m3rlinux.it@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Integer,
+    Transform,
+    Tuple,
+)
+
+from cmk.gui.plugins.wato import (
+    CheckParameterRulespecWithoutItem,
+    rulespec_registry,
+    RulespecGroupCheckParametersApplications,
+)
+
+mailqueue_params = Dictionary(
+    elements=[
+        (
+            "local",
+            Tuple(
+                title=_("Mails injected locally"),
+                help=_("This rule is applied to the number of E-Mails currently "
+                       "in the local mail queue."),
+                elements=[
+                    Integer(title=_("Warning at"), unit=_("mails"), default_value=1000),
+                    Integer(title=_("Critical at"), unit=_("mails"), default_value=2000),
+                ],
+            ),
+        ),
+        (
+            "remote",
+            Tuple(
+                title=_("Mails received remotely"),
+                help=_("This rule is applied to the number of E-Mails currently "
+                       "in the remote mail queue"),
+                elements=[
+                    Integer(title=_("Warning at"), unit=_("mails"), default_value=8000),
+                    Integer(title=_("Critical at"), unit=_("mails"), default_value=10000),
+                ],
+            ),
+        ),
+    ],
+)
+
+
+def _parameter_valuespec_mailqueue_length():
+    return Transform(
+        mailqueue_params,
+        forth=lambda old: not isinstance(old, dict) and {"deferred": old} or old,
+    )
+
+
+rulespec_registry.register(
+    CheckParameterRulespecWithoutItem(
+        check_group_name="qmail_queue_length",
+        group=RulespecGroupCheckParametersApplications,
+        is_deprecated=True,
+        match_type="dict",
+        parameter_valuespec=_parameter_valuespec_mailqueue_length,
+        title=lambda: _("Number of mails in local/remote mail queues"),
+    ))
